3c358c37c8691de9d277fae172956c35ca86b877
sergio.asensio <sasensio@adasasistemas.com>
2026-05-03 22:05:42 +0200

    db: rbac schema removed

--- knexfile.ts
+++ knexfile.ts
@@ -11,7 +11,7 @@ export const knexConfig: Knex.Config = {
     host: process.env.IS_DOCKER ? `${process.env.DOCKER_CONTAINER_NAME}-postgres` : process.env.POSTGRES_HOST,
     timezone: 'UTC'
   },
-  searchPath: ['public', 'rbac', 'master'],
+  searchPath: ['public', 'master'],
   migrations: {
     directory: `db/${process.env.NODE_ENV}/migrations`
   },
